298cbdfb1470be87ccf8caa0a729ccc92ca92e9f
blablo <blablo@gmail.com>
2013-01-02 18:57:17 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mexilist::Application.config.secret_token = '85b43151eff57a828b7bee2f355ac173c4c7a5812ee5772771086fc4758a7f1662df3034c11bd0f6fcb477bbd4ccf6816abbf700079c7c4af5dd2cca11116c89'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mexilist::Application.config.session_store :cookie_store, key: '_mexilist_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mexilist::Application.config.session_store :active_record_store
