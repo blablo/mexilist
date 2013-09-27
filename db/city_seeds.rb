eebb0c6c1266b18fc23461a15a2614b13e85e66a
davidsizu <davidsizu@gmail.com>
2013-09-27 18:48:10 -0500

    Update city_seeds.rb

--- db/city_seeds.rb
+++ db/city_seeds.rb
@@ -3,6 +3,40 @@ State.create_or_update([
                         { :id => 1, :name => 'Distrito Federal'},
                         { :id => 2, :name => 'Estado de México'},
                         { :id => 9, :name => 'Jalisco'}
+                        /*
+                        { :id => 1, :name => 'Aguascalientes'},
+                        { :id => 2, :name => 'Baja California N.'},
+                        { :id => 3, :name => 'Baja California S.'},
+                        { :id => 4, :name => 'Campeche'},
+                        { :id => 5, :name => 'Chiapas'},
+                        { :id => 6, :name => 'Chihuahua'},
+                        { :id => 7, :name => 'Coahuila'},
+                        { :id => 8, :name => 'Colima'},
+                        { :id => 9, :name => 'Distrito Federal'},
+                        { :id => 10, :name => 'Durango'},
+                        { :id => 12, :name => 'Guanajuato'},
+                        { :id => 13, :name => 'Guerrero'},
+                        { :id => 14, :name => 'Hidalgo'},
+                        { :id => 15, :name => 'Jalisco'},
+                        { :id => 11, :name => 'Estado de México'},
+                        { :id => 16, :name => 'Michoacán'},
+                        { :id => 17, :name => 'Morelos'},
+                        { :id => 18, :name => 'Nayarit'},
+                        { :id => 19, :name => 'Nuevo León'},
+                        { :id => 20, :name => 'Oaxaca'},
+                        { :id => 21, :name => 'Puebla'},
+                        { :id => 22, :name => 'Querétaro'},
+                        { :id => 23, :name => 'Quintana Roo'},
+                        { :id => 24, :name => 'San Luis Potosí'},
+                        { :id => 25, :name => 'Sinaloa'},
+                        { :id => 26, :name => 'Sonora'},
+                        { :id => 27, :name => 'Tabasco'},
+                        { :id => 28, :name => 'Tamaulipas'},
+                        { :id => 29, :name => 'Tlaxcala'},
+                        { :id => 30, :name => 'Veracruz'},
+                        { :id => 31, :name => 'Yucatán'},
+                        { :id => 32, :name => 'Zacatecas'}
+*/
                        ])
 
 City.create_or_update([
