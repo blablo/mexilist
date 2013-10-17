8cd5ca963e353c9096a9d538a06367166926ce91
blablo <blablo@gmail.com>
2013-10-16 20:34:14 -0500

    a

--- config/application.rb
+++ config/application.rb
@@ -74,8 +74,8 @@ module Mexilist
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true
-
+#    config.assets.enabled = true
+    config.assets.enabled = false
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
